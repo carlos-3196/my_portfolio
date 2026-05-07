2aaab4fcb2127f9f7ebeb926c0d4a1cc7c2dd01b
Carlos Aguilar <carlos-3196@hotmail.com>
2026-05-07 02:55:16 +0000

    feat: modified header info and summary
    - added profile picture
    - name
    - summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
+import profilePic from './assets/portfolio-picture.jpeg';
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+      <section id="header">
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>Carlos Aguilar</h1>
+          <div className='summary-block'>
+            Results-driven Software Developer with a strong background in UI/UX development, 
+            web design, and full-stack application development
+          </div>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <div className="hero">
+          <img src={profilePic} alt='Profile Picture' />
+        </div>
       </section>
 
       <div className="ticks"></div>
